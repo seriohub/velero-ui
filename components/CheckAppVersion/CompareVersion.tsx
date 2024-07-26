5bb16ba7d9e8357239d7018f5c1c81f84bec2439
DSerio <davide@serio.cloud>
2024-07-26 16:28:54 +0200

    Updated CompareVersion.tsx

--- components/CheckAppVersion/CompareVersion.tsx
+++ components/CheckAppVersion/CompareVersion.tsx
@@ -1,24 +1,26 @@
 export function compareVersions(app: string, lastRelease: string): string {
-    const [major1, minor1, patch1] = app.substring(1).split('.').map(Number);
-    const [major2, minor2, patch2] = lastRelease.substring(1).split('.').map(Number);
-  
-    if (major1 > major2) {
+  if (!app || !lastRelease) return '';
+
+  const [major1, minor1, patch1] = app.substring(1).split('.').map(Number);
+  const [major2, minor2, patch2] = lastRelease.substring(1).split('.').map(Number) || '';
+
+  if (major1 > major2) {
+    return 'app';
+  } else if (major1 < major2) {
+    return 'githubRelease';
+  } else {
+    if (minor1 > minor2) {
       return 'app';
-    } else if (major1 < major2) {
+    } else if (minor1 < minor2) {
       return 'githubRelease';
     } else {
-      if (minor1 > minor2) {
+      if (patch1 > patch2) {
         return 'app';
-      } else if (minor1 < minor2) {
+      } else if (patch1 < patch2) {
         return 'githubRelease';
       } else {
-        if (patch1 > patch2) {
-          return 'app';
-        } else if (patch1 < patch2) {
-          return 'githubRelease';
-        } else {
-          return 'Versions are identical.';
-        }
+        return 'Versions are identical.';
       }
     }
   }
+}
