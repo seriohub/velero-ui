3c751bc64e39e9bc7294bcfc69865fd475dc90a1
DSerio <davide@serio.cloud>
2025-02-07 16:37:30 +0100

    WatchdogService

--- src/components/Actions/ActionsForm/WatchdogService.tsx
+++ src/components/Actions/ActionsForm/WatchdogService.tsx
@@ -2,10 +2,11 @@
 
 import { useEffect, useState } from 'react';
 
-import { Box, Card, Text } from '@mantine/core';
+import { ActionIcon, Box, Card, Group, Text } from '@mantine/core';
 
 import WatchdogServiceConfigTable from '@/components/Inputs/WatchdogServiceConfigTable';
 import { useWatchdogAppSecrets } from '@/api/Watchdog/useWatchdogAppSecrets';
+import { IconInfoSquare } from '@tabler/icons-react';
 
 export default function WatchdogService() {
   const { data: dataServices, getWatchdogAppSecrets } = useWatchdogAppSecrets();
@@ -26,9 +27,21 @@ export default function WatchdogService() {
     <>
       <Card withBorder radius="md" p="xl">
         <Card.Section withBorder inheritPadding p="xs">
-          <Text fz="lg" fw={500}>
-            Service notification
-          </Text>
+          <Group gap={3}>
+            <Text fz="lg" fw={500}>
+              Service notification
+            </Text>
+            <ActionIcon
+              variant="transparent"
+              component="a"
+              href="https://vui.seriohub.com/docs/getting-started/configuration/velero-watchdog"
+              target="_blank"
+              size="xl"
+              aria-label="Open in a new tab"
+            >
+              <IconInfoSquare />
+            </ActionIcon>
+          </Group>
           <Text fz="xs" c="dimmed" mt={3}>
             Config apprise service
           </Text>
